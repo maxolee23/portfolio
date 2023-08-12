e5b466adf201f66398d3ead71211946d155a055f
maxolee23 <maxolee23@gmail.com>
2023-08-11 23:31:07 -0500

    feat: webpack setup (no tailwind yet)

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'index.bundle.js',
+    },
+    devServer: {
+        port: 3010,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ]
+            }
+        ]
+    }
+}
